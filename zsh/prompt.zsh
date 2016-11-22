4dad49ff2d09b18626ef5082bb98759f94a4f5f0
Mike Rodham <switchytm@gmail.com>
2016-11-22 19:14:27 +0000

    Changed terminal icon, upload bug due to gpg sign key

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -78,7 +78,7 @@ prompt_context() {
   local user=`whoami`
 
   if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
-    prompt_segment black default "%(!.%{%F{yellow}%}.)✝"
+    prompt_segment magenta black "%(!.%{%F{yellow}%}.)★  Switchy ★"
   fi
 }
 
